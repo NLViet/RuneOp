c1c21dc20265b70dd0a964ec1ab9d702d29688ef
vnguyen <viet.nguyen@mgm-tp.com>
2015-08-13 15:19:41 +0200

    Added runes

--- client/js/app.js
+++ client/js/app.js
@@ -8,8 +8,26 @@ angular.module('OPRuned', ['ui.router'])
             $scope.monsters = articlesResponse.data;
         });
     })
-    .controller('MonsterDetailCtrl', function($scope,CurrentMonster){
+    .controller('MonsterDetailCtrl', function($scope,CurrentMonster, RuneStorage){
         $scope.currentMonster = CurrentMonster;
+        $scope.runeStorage = RuneStorage;
+        $scope.monster = $scope.currentMonster.getMonster();
+
+        $scope.runes = JSON.parse(localStorage.getItem('runeStore'));
+
+        var temp = JSON.parse(localStorage.getItem('runeStore'));
+        console.log("Lokal");
+        console.log(temp);
+    })
+    .controller('RunesCtrl', function($scope, RuneStorage){
+        $scope.runeStorage = RuneStorage;
+
+        $scope.runeToStore = {};
+
+        $scope.save = function(rune) {
+            $scope.runeToStore = angular.copy(rune);
+            $scope.runeStorage.storeRune($scope.runeToStore);
+        };
     })
     .factory('CurrentMonster', function() {
         var monsterItem;
@@ -19,9 +37,22 @@ angular.module('OPRuned', ['ui.router'])
             },
             setMonster: function(monster) {
                 monsterItem = monster;
-                console.log(monster);
+                localStorage.setItem('newMonster', JSON.stringify(monsterItem));
             },
         };
+    })
+    .factory('RuneStorage', function(){
+        var runes = [];
+        return {
+            storeRune: function(rune){
+                runes.push(rune);
+                localStorage.setItem('runeStore', JSON.stringify(runes));
+                console.log('Runes '+runes);
+            },
+            getRuneStore: function() {
+                return runes;
+            }
+        }
     })
     .config(function($stateProvider, $urlRouterProvider) {
         // For any unmatched url, redirect to /state1
@@ -37,6 +68,10 @@ angular.module('OPRuned', ['ui.router'])
                 templateUrl: "partials/details.html",
                 controller: 'MonsterDetailCtrl'
             })
-
+            .state('runes', {
+                url: "/runes",
+                templateUrl: "partials/runes.html",
+                controller: 'RunesCtrl'
+            })
     });
 
